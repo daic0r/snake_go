7aa199fde89146bb66b3d06c4724c4ca4f62b425
daic0r <daic0r.dev@gmail.com>
2024-03-29 13:53:16 +0100

    Snake can eat and grow

--- snake.go
+++ snake.go
@@ -4,8 +4,8 @@ import (
    "fmt"
    "time"
    "sync"
+   "math/rand"
    rl "github.com/lachee/raylib-goplus/raylib"
-   hook "github.com/robotn/gohook"
 )
 
 const SCREEN_WIDTH = 800;
@@ -29,6 +29,21 @@ type Direction struct {
    val rl.Vector2
 }
 
+type Tile int
+const (
+   Empty = Tile(iota)
+   Food = Tile(iota)
+)
+
+type PlayingField [][]Tile
+func NewPlayingField(w, h int) PlayingField {
+   ret := make([][]Tile, h)
+   for y := 0; y < h; y++ {
+      ret[y] = make([]Tile, w)
+   }
+   return ret
+}
+
 func (this *Direction) Set(x, y float32) {
    this.mut.Lock()
    defer this.mut.Unlock()
@@ -45,13 +60,13 @@ func NewDirection(x, y float32) Direction {
    return Direction{val: rl.NewVector2(x, y)}
 }
 
-func DrawPixel(pos rl.Vector2, pw int, ph int) {
-   rl.DrawRectangle(int(pos.X) * pw, int(pos.Y) * ph, pw, ph, rl.White)
+func DrawPixel(pos rl.Vector2, pw int, ph int, color rl.Color) {
+   rl.DrawRectangle(int(pos.X) * pw, int(pos.Y) * ph, pw, ph, color)
 }
 
 func DrawSnake(s Snake, pw int, ph int) {
    for _, pos := range s {
-      DrawPixel(pos, pw, ph)
+      DrawPixel(pos, pw, ph, rl.White)
       //fmt.Printf("%v\n", pos)
    }
 }
@@ -64,6 +79,8 @@ func (s *Snake) MoveSnake(dir rl.Vector2) {
    *s = (*s)[1:]
    *s = append(*s, (*s)[len(*s) - 1].Add(dir))
 }
+
+var rand_gen = rand.New(rand.NewSource(time.Now().Unix()))
 
 func main() {
    rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake")
@@ -79,42 +96,72 @@ func main() {
    snake = append(snake, rl.NewVector2(11, 10))
    snake = append(snake, rl.NewVector2(12, 10))
 
+   field := NewPlayingField(PIXELS_X, PIXELS_Y)
+
+   occupied_fields := make(map[rl.Vector2]Tile)
+
+   place_food := func() {
+      food_y := rand_gen.Intn(PIXELS_Y)
+      food_x := rand_gen.Intn(PIXELS_X)
+      field[food_y][food_x] = Food
+      occupied_fields[rl.NewVector2(float32(food_x), float32(food_y))] = Food
+   }
+
+   place_food()
+
    dir := Direction{val: rl.NewVector2(1, 0)}
 
-   s := hook.Start()
-   defer hook.End()
+   // s := hook.Start()
+   // defer hook.End()
 
    fmt.Printf("%v %v %v %v\n", Up, Left, Down, Right)
 
-   go func() {
-      for input := range s {
-         if input.Kind == hook.KeyDown {
-            fmt.Printf("%v", input.Rawcode)
-            switch input.Rawcode {
-            case Up:
-               dir.Set(0, -1)
-            case Down:
-               dir.Set(0, 1)
-            case Left:
-               dir.Set(-1, 0)
-            case Right:
-               dir.Set(1, 0)
-            }
-         }
-      }
-   }()
-
+   duration := 300
+   score := 0
    for !rl.WindowShouldClose() {
-      rl.BeginDrawing()
-
-      rl.ClearBackground(rl.Black)
-
-      DrawSnake(snake, pixel_width, pixel_height)
+      if rl.IsKeyDown(rl.KeyUp) {
+         dir.Set(0, -1)
+      } else
+      if rl.IsKeyDown(rl.KeyDown) {
+         dir.Set(0, 1)
+      } else
+      if rl.IsKeyDown(rl.KeyLeft) {
+         dir.Set(-1, 0)
+      } else
+      if rl.IsKeyDown(rl.KeyRight) {
+         dir.Set(1, 0)
+      }
 
       snake.MoveSnake(dir.Get())
 
-      time.Sleep(500 * time.Millisecond)
+      for pos := range occupied_fields {
+         last := snake[len(snake) - 1]
+         if pos == last {
+            delete(occupied_fields, pos)
+            append_dir := last.Subtract(snake[len(snake) - 2])
+            fmt.Printf("%v\n", append_dir)
+            snake = append(snake, last.Add(append_dir))
+            place_food()
+            score += 50
+         }
+      }
+
+      rl.BeginDrawing()
+
+         rl.ClearBackground(rl.Black)
+
+         DrawSnake(snake, pixel_width, pixel_height)
+         for pos, t := range occupied_fields {
+            switch t {
+            case Food:
+               DrawPixel(pos, pixel_width, pixel_height, rl.Green)
+            default:
+               DrawPixel(pos, pixel_width, pixel_height, rl.Red)
+            }
+         }
 
       rl.EndDrawing()
+
+      time.Sleep(time.Duration(duration) * time.Millisecond)
    }
 }
